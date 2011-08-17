17677ebfcfc53668be853a72510c84dca862bffb
Peter Waller <peter.waller@cern.ch>
2011-08-17 10:31:41 +0100

    WIP

--- weboot/resources.py
+++ weboot/resources.py
@@ -2,7 +2,11 @@
 from os import listdir
 from os.path import exists, isfile, isdir, join as pjoin
 
+import re
+import fnmatch
+
 from pyramid.httpexceptions import HTTPNotFound
+from pyramid.traversal import traverse
 
 import ROOT as R
 
@@ -19,7 +23,6 @@ class LocationAware(object):
         return c
         
 class Root(dict, LocationAware):
-
     def __init__(self, request):
         self.request = request
         self['result'] = FilesystemTraverser.from_parent(self, "result")
@@ -28,29 +31,63 @@ class RootObject(LocationAware):
     def __init__(self, request, root_object):
         self.request = request
         self.o = root_object
-        
-    def __repr__(self):
-        return '<RootObject h="{0}">'.format(self.o)
 
-class RootFileTraverser(LocationAware):
+class DirectoryTraverser(object):
+    pass
+
+class RootFileTraverser(LocationAware, DirectoryTraverser):
     def __init__(self, request, rootfile):
         self.request, self.rootfile = request, rootfile
+        
+    @property
+    def path(self):
+        return self.rootfile.GetPath()
         
     @property
     def content(self):
         keys = [k.GetName() for k in self.rootfile.GetListOfKeys()]
         def link(p):
             url = self.request.resource_url(self, p)
-            return '<p><a href="{0}">{1}</a></p>'.format(url, p)
+            return '<p><a href="{0}">{1}</a><img src="{0}" width="10%" height="10%"/></p>'.format(url, p)
         return "".join(link(p) for p in keys)
         
     def __getitem__(self, subpath):
+        print "Traversing root object at", subpath
+        if "*" in subpath:
+            keys = [l.GetName() for l in self.rootfile.GetListOfKeys()]
+            pattern = re.compile(fnmatch.translate(subpath))
+            print "Matching keys:", [f for f in keys if pattern.match(f)]
+            contexts = [(f, traverse(self, f)["context"])
+                        for f in keys if pattern.match(f)]
+            return MultipleTraverser.from_parent(self, subpath, contexts)
         leaf = self.rootfile.Get(subpath)
         if not leaf:
             raise HTTPNotFound()
         if isinstance(leaf, R.TDirectory):
             return RootFileTraverser.from_parent(self, subpath, leaf)
+            
         return RootObject.from_parent(self, subpath, leaf)
+
+class MultipleTraverser(LocationAware, DirectoryTraverser):
+    def __init__(self, request, contexts):
+        self.request = request
+        self.contexts = contexts
+    
+    @property
+    def path(self):
+        return "MultipleTraverser"
+    
+    @property
+    def content(self):
+        return "Hello! I have some contexts.."
+    
+    def __getitem__(self, subpath):
+        print "Attempting to traverse {0} contexts at {1}".format(len(self.contexts), subpath)
+        new_contexts = [(f, traverse(c, subpath)["context"])
+                        for f, c in self.contexts]
+        if all(x is None for f, x in new_contexts):
+            raise HTTPNotFound("Failed to traverse at {0}".format(subpath))
+        return MultipleTraverser.from_parent(self, subpath, new_contexts)
 
 class FilesystemTraverser(LocationAware):
     def __init__(self, request):
@@ -62,15 +99,26 @@ class FilesystemTraverser(LocationAware):
         def link(p):
             url = self.request.resource_url(self, p)
             return '<p><a href="{0}">{1}</a></p>'.format(url, p)
-        return "".join(link(p) for p in listdir(self.path))
+        return "".join(link(p) for p in self.ls)
+    
+    @property
+    def ls(self):
+        return sorted(listdir(self.path))
     
     def __getitem__(self, subpath):
-        path = self.path = pjoin(self.path, subpath)
-        #print "Attempting traversal on", path, subpath
+        path = pjoin(self.path, subpath)
         if isfile(path):
             f = R.TFile(path)
             if f.IsZombie() or not f.IsOpen():
                 raise HTTPNotFound("Failed to open {0}".format(path))
             return RootFileTraverser.from_parent(self, subpath, f)
-        return FilesystemTraverser.from_parent(self, subpath, path=self.path)
+            
+        elif isdir(path):
+            return FilesystemTraverser.from_parent(self, subpath, path=path)
+            
+        elif "*" in subpath:
+            pattern = re.compile(fnmatch.translate(subpath))
+            contexts = [(f, traverse(self, f)["context"])
+                        for f in listdir(self.path) if pattern.match(f)]
+            return MultipleTraverser.from_parent(self, subpath, contexts)
 
--- weboot/views.py
+++ weboot/views.py
@@ -1,29 +1,46 @@
 from os.path import exists, join as pjoin
 from tempfile import NamedTemporaryFile
 
-from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPNotFound
 from pyramid.response import Response
+from pyramid.view import view_config
 
 import ROOT as R
 
 from minty.histograms import fixup_hist_units
 
-from .resources import FilesystemTraverser, RootFileTraverser, RootObject
+from .resources import MultipleTraverser, FilesystemTraverser, RootFileTraverser, RootObject
 
 def my_view(request):
     
     print "Test!", R.kTRUE
     
     return {'project':'WebOOT'}
-    
+
+def build_draw_params(params):
+    options = []
+    if "hist" in params:
+        options.append("hist")
+    return " ".join(options)
     
 @view_config(renderer='weboot:templates/result.pt', context=RootObject)
-def view_result(context, request):    
-    c = R.TCanvas()
-    c.SetLogy()
+def view_result(context, request):
     h = context.o
+    if not isinstance(h, R.TH1):
+        raise HTTPNotFound("Not a histogram")
+    
+    print "Will attempt to render", h
+    if isinstance(h, R.TH3):
+        h = h.Project3D("x")
+    if isinstance(h, R.TH2):
+        raise HTTPNotFound
+    c = R.TCanvas()
+    if "log" in request.params:
+        c.SetLogy()
+    #if h.Get
     h = fixup_hist_units(h)
-    h.Draw()
+    
+    h.Draw(build_draw_params(request.params))
     
     with NamedTemporaryFile(suffix=".png") as tmpfile:
         c.SaveAs(tmpfile.name)
@@ -31,9 +48,17 @@ def view_result(context, request):
         
     return Response(content, content_type="image/png")
     
+@view_config(renderer='weboot:templates/result.pt', context=MultipleTraverser)
+def view_multitraverse(context, request):
+    content = []
+    for c in context.contexts:
+        content.append("<p>{repr(0)}</p>".format(c))
+    return dict(path="You are at {0}".format(context.path),
+                content="\n".join(content))
+
 @view_config(renderer='weboot:templates/result.pt', context=RootFileTraverser)
 def view_rootfile(context, request):
-    return dict(path="You are at {0}".format(context.rootfile.GetPath()),
+    return dict(path="You are at {0}".format(context.path),
                 content=context.content)
     
 @view_config(renderer='weboot:templates/result.pt', context=FilesystemTraverser)
