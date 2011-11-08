5fca6a761deed72bd358c8b4a779a73c5e08e833
Peter Waller <peter.waller@cern.ch>
2011-11-08 12:51:04 +0000

    Remove histogram and stackplot submodules, make them plain python files

--- weboot/resources/root/histogram/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Avoid putting imports here. It hides information and can cause circular import problems.
-"""
-from .. import log; log = log.getChild(__name__)
-
-from .histogram import Histogram, FreqHist, HistogramTable
