a93932e0793192a9b517169ae791622174d734a9
Peter Waller <peter@pwaller.net>
2012-11-17 23:23:57 +0000

    Add resource which shows current stack

--- weboot/resources/home.py
+++ weboot/resources/home.py
@@ -3,16 +3,44 @@ This file contains the Home object
 """
 
 from pyramid.httpexceptions import HTTPFound
+from pyramid.response import Response
 from pyramid.security import authenticated_userid
 
 from .actions import HasActions
 from .locationaware import LocationAware
+from .renderable import Renderer
 from .user import UserResource
 
 # ** Additional imports appear below to avoid circular dependencies ** #
 class EnvResource(LocationAware):
     def __init__(self, request):
         self.request = request
+
+class StackResource(Renderer):
+    def __init__(self, request):
+        self.request = request
+
+    @property
+    def content(self):
+
+        import sys, traceback
+
+        frames = sorted(sys._current_frames().iteritems())
+
+        active, waiting = [], []
+        for thread_id, f in frames:
+
+            if "wait" in f.f_code.co_name:
+                waiting.append("  {0}".format(thread_id))
+                continue
+
+            s = "".join(traceback.format_stack(f))
+            stack = "Thread: {0}\n{1}".format(thread_id, s)
+            active.append(stack)
+
+        response = "Current stack (ignoring {1} waiting):\n\n{0}".format(
+            "\n\n".join(active), len(waiting))
+        return Response(response, content_type="text/plain")
 
 
 class HomeResource(LocationAware, dict):
@@ -21,7 +49,7 @@ class HomeResource(LocationAware, dict):
         
         self.add("browse",  VFSTraverser)
         self.add("env",     EnvResource)
-        
+        self.add("stack",   StackResource)
 
         if self.request.db:
             self.add("baskets", BasketBrowser)
